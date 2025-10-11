671de3c8f7f673ff4a453c8f29c2fdbe27e728ee
Cassia Mill <kssia.mill@gmail.com>
2025-10-11 15:57:42 -0400

    Moved folders

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// This is where the app starts
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Testing hackathon frontend</h1>);
